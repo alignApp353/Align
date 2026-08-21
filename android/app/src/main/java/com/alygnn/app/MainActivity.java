0c27cbb84ec653af04d4cb090edda8b74565a4da
alignApp353 <laraauyrr@gmail.com>
2026-08-20 21:59:12 -0400

    Update MainActivity.java

--- android/app/src/main/java/com/alygnn/app/MainActivity.java
+++ android/app/src/main/java/com/alygnn/app/MainActivity.java
@@ -1,5 +1,22 @@
 package com.alygnn.app;
 
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatDelegate;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Apply the phone's Light/Dark setting before Capacitor creates
+        // the WebView. This fixes the first cold-start page opening light
+        // and only switching to dark after navigating.
+        AppCompatDelegate.setDefaultNightMode(
+            AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+        );
+
+        super.onCreate(savedInstanceState);
+    }
+}
